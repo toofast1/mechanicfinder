df3e0e839b628b83d85ff86a258e42e8f14e4b63
Daniel Zelaya <wupaz9@gmail.com>
2017-08-26 23:49:09 -0600

    Banner for main page

--- src/MechanicFinder.js
+++ src/MechanicFinder.js
@@ -3,11 +3,16 @@ import React, { Component } from 'react';
 import LogoBar from './components/LogoBar';
 import SearchBox from './components/SearchBox';
 
+import styles from './MechanicFinder.css';
+
 class MechanicFinder extends Component {
   render() {
     return (
       <div>
         <LogoBar />
+        <div className={styles["banner"]}>
+          <p>Encuentra tu taller al mejor precio</p>
+        </div>
         <SearchBox />
       </div>
     );
