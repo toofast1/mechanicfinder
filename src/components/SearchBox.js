8b715b1ef018992b55ec5cd8d6dc28813d612f1f
Daniel Zelaya <wupaz9@gmail.com>
2017-08-31 13:50:26 -0600

    Funcitonal search box

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, withRouter } from 'react-router-dom';
 
 import styles from './SearchBox.css';
 
-class SearchBox extends Component {
-  render() {
-    return (
-      <div className={styles["root"]}>
-        <input type="text" placeholder="e.g El Mejor Taller" />
-        <button>Search</button>
-      </div>
-    );
-  }
+const linkStyle = { color: "#fff", textDecoration: "none" }; 
+
+function cleanQueryStr(str) {
+  return str.split('').filter(c => c != '/').join("");
 }
+
+function search(history, value) {
+  history.push(`/${cleanQueryStr(value)}`);
+}
+
+const Input = withRouter(({ value, onChange, history }) => (
+  <input type="text" placeholder="e.g El Mejor Taller" 
+        value={value} onChange={onChange}
+         onKeyPress={e => { if (e.which == 13) search(history, value); }} autoFocus />
+));
+
+const Button = withRouter(({ history, searchText, children }) => (
+  <button onClick={() => search(history, searchText)} style={{ cursor: 'pointer' }}>{children}</button>
+));
+
+const SearchBox = ({ className = '', style = {}, value, onChange }) => (
+  <div className={`${styles.root} ${className}`} style={style}>
+    <Input value={value} onChange={onChange} />
+    <Button searchText={value}>Search</Button>
+  </div>
+);
+
+SearchBox.propTypes = {
+  className: PropTypes.string,
+  style: PropTypes.object,
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
 
 export default SearchBox;
