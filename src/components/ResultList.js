ea4d147ccb5550b8b7bc6b62701bc0e03bded152
Daniel Zelaya <wupaz9@gmail.com>
2017-08-31 13:53:04 -0600

    Add ResultList component

---
+++ src/components/ResultList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, withRouter } from 'react-router-dom';
+
+import Result from './Result';
+
+import styles from './ResultList.css';
+
+const ResultList = ({ className = '', style = {}, results = [] }) => (
+  <div className={`${styles.root} ${className}`} style={style}>
+    {results.map((e, i) => (<Result key={i} data={e} />))}
+  </div>
+);
+
+ResultList.propTypes = {
+  className: PropTypes.string,
+  style: PropTypes.object,
+  results: PropTypes.arrayOf(PropTypes.object)
+};
+
+export default ResultList;
