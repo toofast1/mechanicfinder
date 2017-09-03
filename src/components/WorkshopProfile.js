2c618bb20494465dd21fd6542a15f6af86af1980
Daniel Zelaya <wupaz9@gmail.com>
2017-09-03 03:23:20 -0600

    Responsive styles for workshop profile

--- src/components/WorkshopProfile.js
+++ src/components/WorkshopProfile.js
@@ -44,27 +44,50 @@ class WorkshopProfile extends Component {
       <div className={styles.root}>
         {/* Back to Results Button Here */}
         <div className={styles["profile-section"]}>
-          <div className={styles["img-area"]}>
+          <div className={styles.header}>
             <img src={pic} alt="Foto del taller" />
+            <div className={styles["header-info"]}>
+              <h2>{workshop.name}</h2>
+              <p className={styles.location}>{workshop.location}</p>
+              <StarRating
+                starCount={5}
+                starFillColor="#0173bd"
+                starStrokeColor="#b7b7b7"
+                starWidth={18}
+                starHeight={18}
+                value={Math.round(rating)}
+                disabled
+              />
+            </div>
           </div>
           <div className={styles.details}>
-            <h2>{workshop.name}</h2>
-            <p className={styles.location}>{workshop.location}</p>
-            <StarRating
-              starCount={5}
-              value={Math.round(rating)}
-              disabled
-            />
             <Separator />
-            <h4>Sobre Este Taller</h4>
-            <p>{workshop.description}</p>
+            <div className={styles["detail-section"]}>
+              <h4>Sobre Este Taller</h4>
+              <p className={styles.description}>{workshop.description}</p>
+            </div>
             <Separator />
-            <h4>Horas de Atencion</h4>
+            <div className={styles["detail-section"]}>
+              <h4>Horas de Atencion</h4>
+              <p className={styles["shop-hours"]}>
+                <b>{workshop.shopHours.days}</b>
+                <span>{workshop.shopHours.hours}</span>
+              </p>
+            </div>
             <Separator />
-            <h4>Servicios</h4>
-            {workshop.keywords.map((k, i) => <p key={i}>{k}</p>)}
+            <div className={styles["detail-section"]}>
+              <h4>Servicios</h4>
+              <ul>
+                {workshop.keywords.map((k, i) => <li key={i}>{k}</li>)}
+              </ul>
+            </div>
             <Separator />
-            <h4>Tipos de Pago</h4>
+            <div className={styles["detail-section"]}>
+              <h4>Tipos de Pago</h4>
+              <ul>
+                {workshop.paymentTypes.map((p, i) => <li key={i}>{p}</li>)}
+              </ul>
+            </div>
             <Separator />
           </div>
         </div>
