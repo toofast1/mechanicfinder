6c8085500f09622040b0e72fbaf67c914d4dc52d
Daniel Zelaya <wupaz9@gmail.com>
2017-09-03 05:56:30 -0600

    Custom width

--- src/components/Separator.js
+++ src/components/Separator.js
@@ -3,12 +3,18 @@ import PropTypes from 'prop-types';
 
 const DEFAULT_COLOR = '#ccc';
 const DEFAULT_MARGIN = 13;
+const DEFAULT_WIDTH = 1;
 
-const Separator = ({ color = DEFAULT_COLOR, marginBottom = DEFAULT_MARGIN, marginTop = DEFAULT_MARGIN }) => (
+const Separator = ({ 
+    color = DEFAULT_COLOR, 
+    marginBottom = DEFAULT_MARGIN, 
+    marginTop = DEFAULT_MARGIN, 
+    width = DEFAULT_WIDTH 
+}) => (
     <div
         style={{
             backgroundColor: color,
-            height: 1,
+            height: width,
             marginBottom,
             marginTop
         }}
@@ -18,7 +24,8 @@ const Separator = ({ color = DEFAULT_COLOR, marginBottom = DEFAULT_MARGIN, margi
 Separator.propTypes = {
     color: PropTypes.string,
     marginBottom: PropTypes.number,
-    marginTop: PropTypes.number
+    marginTop: PropTypes.number,
+    width: PropTypes.number
 };
 
 export default Separator;
