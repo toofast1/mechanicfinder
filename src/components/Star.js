3fbb2a32da8aacec98b52033736a3a38d18a6006
Daniel Zelaya <wupaz9@gmail.com>
2017-09-03 03:19:19 -0600

    Allow star fill and stroke color to be editted along with width and height

--- src/components/Star.js
+++ src/components/Star.js
@@ -4,14 +4,19 @@ import PropTypes from 'prop-types';
 import FaStar from 'react-icons/fa/star';
 import FaStarO from 'react-icons/fa/star-o';
 
-const Star = ({ colored, ...props }) => (
+const Star = ({ colored, fillColor, strokeColor, width = 14, height = 14, ...props }) => (
   <div {...props}>
-    { colored ? <FaStar /> : <FaStarO /> }
+    { colored ? 
+      <FaStar  style={ fillColor ? { color: fillColor, width, height } : { width, height } } /> : 
+      <FaStarO style={ strokeColor ? { color: strokeColor, width, height } : { width, height } } /> 
+    }
   </div>
 );
 
 Star.propTypes = {
-  colored: PropTypes.bool
+  colored: PropTypes.bool,
+  fillColor: PropTypes.string,
+  strokeColor: PropTypes.string
 }
 
 export default Star;
